d1fd5d7006e7f73d4be849a8e10bbc9baf9e0898
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-03-01 20:39:34 -0600

    Realice un  ejercicio con funciones

---
+++ funcionesCarro.py
@@ -0,0 +1,12 @@
+
+def velocidad(d, v):
+    t = d/v
+    return t
+
+
+def mensaje(tiempo):
+    print("Su tiempo  es de : ", tiempo, " horas")
+
+ 
+t = velocidad(100, 50)
+mensaje(t)
---
+++ hola.py
@@ -0,0 +1,20 @@
+print("Hola mundo ")
+
+# Comentario
+'''Comentario 
+de varias lineas 
+'''
+mensaje = "Bienvenidos a Python"
+print(mensaje)
+#Cambiamos el valor de la variable mensaje
+mensaje = "Bienvenidos a Python 3"
+print(mensaje)
+
+# Tipos de datos numericos
+numero = 10
+numero2 = "10"
+numero3 = 12.645
+total = numero + int(numero2)
+print(type(total))
+print(type(numero2))
+print(type(numero3))
