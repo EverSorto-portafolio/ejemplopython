333d9950ac2340c606ae9cb53f87c7baae9487b4
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-03-17 22:50:19 -0600

    ejecicio sobre los descuentos en python

---
+++ descuento.py
@@ -0,0 +1,38 @@
+#- si la compra es mayor a
+#    - 50 → 0.05
+#    - 100 → 0.10
+#    - 200 → 0.12
+#    - 1000 → 0.2
+#- Mostrar
+#- total real
+#- total descuento
+#- total a pagar
+
+compra = float(input("Ingrese el monto de compra "))
+descuento = 0
+
+def mostrar(compra, descuento):
+    
+    if compra < 50 : 
+        print("Total sin descuento " , compra)
+        print("Descuento :  no aplica")  
+        print("Total a pagar " , compra)
+    else:
+        descuento = compra * descuento
+        total = compra - descuento 
+        print("Total sin descuento " , compra)
+        print("Descuento :  " , descuento)  
+        print("Total a pagar " , total)
+
+if compra >= 50  and compra < 100:
+    descuento = 0.05
+
+if compra >= 100  and compra < 100:
+    descuento = 0.10
+
+if compra >= 100  and compra < 200:
+    descuento = 0.12
+
+if compra >= 200  and compra < 1000:
+    descuento = 0.2
+mostrar(compra, descuento)
