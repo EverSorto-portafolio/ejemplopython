62542a9080ca5a78d2716f8e98eded87f98faac6
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-03-19 22:40:03 -0600

    uso de cadenas de texto salto de linea finalizado

---
+++ UsoString.py
@@ -0,0 +1,23 @@
+# uso simple 
+simple = "Soy un texto simple"
+
+#texto multilinea
+textoGrande = ''' Tengo muchas lineas 
+                muuchas lineas 
+ '''
+#Concatenar (Unir text0)
+unir = simple + textoGrande
+#multplicar texto|
+unir = simple * 2
+print(unir)
+
+print(" lineas  1 \n linea 2 \n linea 3")
+print(" tabulador \t tabulador")
+print(" retroceso \b retroceso")
+print(" comilla simple \' comilla simple")
+print(" comilla doble \" comilla doble")
+print(" barra invertida \\ barra invertida")
+print(" salto de pagina \f salto de pagina")
+print(" retorno de carro \r retorno de carro")
+print(" salto de linea \n salto de linea")
+
